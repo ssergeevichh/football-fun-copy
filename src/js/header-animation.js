03c7169853fd279fb8be3cea6915b256b030c2ce
Vladyslav Kuidin <vkuydi@gmail.com>
2022-03-14 20:33:44 +0200

    feat:full adaptive header

--- src/js/header-animation.js
+++ src/js/header-animation.js
@@ -3,46 +3,75 @@ const searchBtn = document.querySelector('.icon-search')
 const menu = document.querySelector('.menu-wrapper')
 const menuBtn = document.querySelector('.hamburger')
 const menuLayer = document.querySelector('.menu-overlay')
-const mobileMenu = document.querySelector('.mobile-menu')
+const mobileMenu = document.querySelector('.mobile-menu-wrapper')
 const closeMenuBtn = document.querySelector('.menu-close')
+const mobileCloseBtn = document.querySelector('.header-line-mobile__arrow')
+const mobileSearchBtn = document.querySelector('.icon-search-mobile')
+const searchBlock = document.querySelector('.header-search')
 
-searchInput.addEventListener('focusin', () => {
-    searchBtn.style.padding = 5 + 'px'
-})
+function hideElement(...elements) {
+    for (let elem of elements) {
+        elem.style.opacity = 0
+        elem.style.visibility = 'hidden'
+    }
+}
 
-searchInput.addEventListener('focusout', () => {
-    searchBtn.style.padding = ''
-})
+function showElement(...elements) {
+    for (let elem of elements) {
+        elem.style.opacity = 1
+        elem.style.visibility = 'visible'
+    }
+}
 
-menuBtn.addEventListener('click', () => {
-    mobileMenu.classList.add('mobile-menu-active')
-
-    menuBtn.classList.add('hamburger-non-active')
-
-    closeMenuBtn.classList.add('menu-close-active')
-
-    menuLayer.style.opacity = 1
-    menuLayer.style.visibility = 'visible'
-
-})
-
-menuLayer.addEventListener('click', () => {
-    mobileMenu.classList.remove('mobile-menu-active')
-
-    setTimeout(() => menuBtn.classList.remove('hamburger-non-active'),190)
-
-    closeMenuBtn.classList.remove('menu-close-active')
-
-
-    menuLayer.style.opacity = 0
-    menuLayer.style.visibility = ''
-})
-
-window.addEventListener('scroll',() => {
+// fixed menu
+window.addEventListener('scroll', () => {
     if (window.scrollY > 1) {
         menu.classList.add('scrolled-menu')
     } else {
         menu.classList.remove('scrolled-menu')
     }
 
+})
+
+//searching input animation
+searchInput.addEventListener('focusin', () => {
+    searchBtn.classList.add('icon-search__active')
+})
+
+searchInput.addEventListener('focusout', () => {
+    searchBtn.classList.remove('icon-search__active')
+
+})
+
+//mobile version of menu
+menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.add('mobile-menu-active')
+
+    hideElement(menuBtn)
+
+    showElement(closeMenuBtn, menuLayer)
+
+})
+
+//mobile version of menu
+document.addEventListener('click', ({target}) => {
+    if(target === menuLayer || target === mobileCloseBtn) {
+        mobileMenu.classList.remove('mobile-menu-active')
+        
+        setTimeout(() => showElement(menuBtn), 190)
+        
+        hideElement(closeMenuBtn, menuLayer)
+    }
+})
+
+//mobile searching block
+mobileSearchBtn.addEventListener('click', () => {
+    hideElement(mobileSearchBtn)
+    showElement(searchBlock)
+    document.addEventListener('click', ({target}) => {
+        if(target !== searchInput && !target.closest('.icon-search-mobile') && !target.closest('.icon-search')) {
+            hideElement(searchBlock)
+            showElement(mobileSearchBtn )
+        }
+    })
 })

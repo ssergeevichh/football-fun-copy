552a82bb984c54a708366fd9b0fbf47b781e409a
Vladyslav Kuidin <vkuydi@gmail.com>
2022-03-29 21:58:37 +0300

    refac:change type of event in forms

--- src/js/voting.js
+++ src/js/voting.js
@@ -2,7 +2,9 @@ import { hideElement, showElement } from './helper'
 import { sendPoll } from './server'
 
 const votingBlock = document.querySelector('.voting-forms')
+const forms = document.querySelectorAll('.voting-form')
 const rejectedMessage = document.querySelector('.voting-forms__error-message')
+
 
 let poll = {};
 
@@ -13,74 +15,80 @@ votingBlock.addEventListener('change', ({ target }) => {
     }
 })
 
-let voting = (e) => {
-    if (e.target.closest('[data-id="vote-btn"]')) {
-        e.preventDefault();
-        const submitVoteBtn = e.target
-        const currentPoll = e.target.closest('.voting')
 
-        const resultItems = currentPoll.querySelectorAll('.voting-result')
-        const answerVariantsBlock = currentPoll.querySelector('.voting__variants-wrapper')
-        const preloader = currentPoll.querySelector('.voting__preload')
-        const votingResultsBlock = currentPoll.querySelector('.voting__results-wrapper')
 
-        if (currentPoll.lastChild === rejectedMessage) {
-            rejectedMessage.style.display = 'none'
-            currentPoll.removeChild(rejectedMessage)
-        }
+let sendForm = (e) => {
+    e.preventDefault();
+    const currentForm = e.target
+    const currentPoll = currentForm.querySelector('.voting')
 
-        if (Object.keys(poll).length !== 0 && poll.pollId === e.target.closest('.voting-form').id) {
-            submitVoteBtn.classList.add('btn--non-active')
+    const submitVoteBtn = currentPoll.querySelector('[data-id="vote-btn"]')
+    const resultItems = currentPoll.querySelectorAll('.voting-result')
+    const answerVariantsBlock = currentPoll.querySelector('.voting__variants-wrapper')
+    const preloader = currentPoll.querySelector('.voting__preload')
+    const votingResultsBlock = currentPoll.querySelector('.voting__results-wrapper')
 
-            answerVariantsBlock.classList.add('voting__variants-wrapper--filter-blur')
-            showElement(preloader)
 
-            sendPoll(poll)
-                .then(data => {
-                    poll = {}
+    if (currentPoll.lastChild === rejectedMessage) {
+        rejectedMessage.style.display = 'none'
+        currentPoll.removeChild(rejectedMessage)
+    }
 
-                    submitVoteBtn.setAttribute('disabled', 'disabled')
+    if (Object.keys(poll).length !== 0 && poll.pollId === e.target.closest('.voting-form').id) {
+        submitVoteBtn.classList.add('btn--non-active')
 
-                    hideElement(preloader, answerVariantsBlock)
-                    showElement(votingResultsBlock)
+        answerVariantsBlock.classList.add('voting__variants-wrapper--filter-blur')
+        showElement(preloader)
 
-                    answerVariantsBlock.classList.remove('voting__variants-wrapper--filter-blur')
-                    answerVariantsBlock.classList.add('voting__variants-wrapper--non-active')
+        sendPoll(poll)
+            .then(data => {
+                poll = {}
 
-                    votingResultsBlock.style.display = 'block'
+                submitVoteBtn.setAttribute('disabled', 'disabled')
 
-                    let votes = data.votes.reduce((previousValue, currentValue) => previousValue + currentValue, 0)
+                hideElement(preloader, answerVariantsBlock)
+                showElement(votingResultsBlock)
 
-                    resultItems.forEach((btn, index) => {
-                        const currentVariantVotes = data.votes[index]
-                        const resultItemIndicators = btn.querySelector('.state-inner')
-                        const indicatorPercent = btn.querySelector('.state-inner__percent')
-                        const indicatorQuantity = btn.querySelector('.state-inner__quantity')
-                        const progressLine = btn.querySelector('.voting-result__line')
-                        const individualPercentNum = Math.round(currentVariantVotes * 100 / votes)
+                answerVariantsBlock.classList.remove('voting__variants-wrapper--filter-blur')
+                answerVariantsBlock.classList.add('voting__variants-wrapper--non-active')
 
-                        showElement(resultItemIndicators)
+                votingResultsBlock.style.display = 'block'
 
-                        indicatorPercent.innerHTML = individualPercentNum
-                        indicatorQuantity.innerHTML = currentVariantVotes
+                let votes = data.votes.reduce((previousValue, currentValue) => previousValue + currentValue, 0)
 
-                        setTimeout(() => progressLine.style.width = individualPercentNum + '%', 170)
+                resultItems.forEach((btn, index) => {
+                    const currentVariantVotes = data.votes[index]
+                    const resultItemIndicators = btn.querySelector('.state-inner')
+                    const indicatorPercent = btn.querySelector('.state-inner__percent')
+                    const indicatorQuantity = btn.querySelector('.state-inner__quantity')
+                    const progressLine = btn.querySelector('.voting-result__line')
+                    const individualPercentNum = Math.round(currentVariantVotes * 100 / votes)
 
-                    })
+                    showElement(resultItemIndicators)
 
-                }, reason => {
-                    rejectedMessage.style.display = 'block'
-                    rejectedMessage.innerHTML = reason
-                    currentPoll.appendChild(rejectedMessage)
+                    indicatorPercent.innerHTML = individualPercentNum
+                    indicatorQuantity.innerHTML = currentVariantVotes
 
-                    submitVoteBtn.classList.remove('btn--non-active')
-
-                    answerVariantsBlock.classList.remove('voting__variants-wrapper--filter-blur')
-
-                    hideElement(preloader)
+                    setTimeout(() => progressLine.style.width = individualPercentNum + '%', 170)
 
                 })
-        }
+
+            }, reason => {
+                rejectedMessage.style.display = 'block'
+                rejectedMessage.innerHTML = reason
+                currentPoll.appendChild(rejectedMessage)
+
+                submitVoteBtn.classList.remove('btn--non-active')
+
+                answerVariantsBlock.classList.remove('voting__variants-wrapper--filter-blur')
+
+                hideElement(preloader)
+
+            })
     }
 }
-votingBlock.addEventListener('click', voting)
+
+
+forms.forEach(form => {
+    form.addEventListener('submit', sendForm)
+})
